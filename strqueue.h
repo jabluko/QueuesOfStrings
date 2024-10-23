aa0ec16931ce78a2a7418b66b07644b852804a52
jabluko <cs.pawelmieszkowski@gmail.com>
2024-10-23 09:38:40 +0200

    better header

--- strqueue.h
+++ strqueue.h
@@ -1,19 +1,25 @@
 #ifndef _STRQUEUE_H_
 #define _STRQUEUE_H_
-#include <cstddef>
 
-extern "C"
-{
-    namespace cxx
-    {
-        unsigned long strqueue_new();
-        void strqueue_delete(unsigned long id);
-        size_t strqueue_size(unsigned long id);
-        void strqueue_remove_at(unsigned long id, size_t position);
-        const char* strqueue_get_at(unsigned long id, size_t position);
-        void strqueue_clear(unsigned long id);
-        int strqueue_comp(unsigned long id1, unsigned long id2);
+#ifndef __cplusplus
+#include <stddef.h>
+#else
+#include <cstddef>
+extern "C" {
+    namespace cxx {
+#endif
+
+unsigned long strqueue_new();
+void strqueue_delete(unsigned long id);
+size_t strqueue_size(unsigned long id);
+void strqueue_remove_at(unsigned long id, size_t position);
+const char* strqueue_get_at(unsigned long id, size_t position);
+void strqueue_clear(unsigned long id);
+int strqueue_comp(unsigned long id1, unsigned long id2);
+
+#ifdef __cplusplus
     }
 }
+#endif
 
 #endif
